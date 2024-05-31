f41bf166389769193edc448e4c50850f8d20dabd
Jeff Shuo <jeffshuo@secuxtech.com>
2024-05-31 13:12:57 +0800

    did puf initial

---
+++ packages/nextjs/app/issuer/page.tsx
@@ -0,0 +1,49 @@
+// app/issuer/page.tsx
+"use client";
+
+import Head from "next/head";
+
+// app/issuer/page.tsx
+
+// app/issuer/page.tsx
+
+// app/issuer/page.tsx
+
+// app/issuer/page.tsx
+
+// app/issuer/page.tsx
+
+// app/issuer/page.tsx
+
+// app/issuer/page.tsx
+
+// app/issuer/page.tsx
+
+// app/issuer/page.tsx
+
+// app/issuer/page.tsx
+
+export default function IssuerPage() {
+  return (
+    <div>
+      <Head>
+        <title>Simple Next.js Page</title>
+      </Head>
+      <main>
+        <h1>Welcome to the Issuer Page</h1>
+        <p>This is a simple Next.js page.</p>
+        <ul>
+          <li>First item in the list</li>
+          <li>Second item in the list</li>
+          <li>Third item in the list</li>
+        </ul>
+        <a href="/">
+          <button>Home</button>
+        </a>
+        <a href="/audience">
+          <button>Audience App</button>
+        </a>
+      </main>
+    </div>
+  );
+}
--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import type { NextPage } from "next";
+import { FaUsb } from "react-icons/fa";
+import { SiEthereum } from "react-icons/si";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
@@ -15,7 +17,8 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">IOTA EVM NeoPUF-based Decentralized Identity App</span>
+            <span className="block text-4xl font-bold">For Enterprise Supply Chain Management</span>
           </h1>
           <div className="flex justify-center items-center space-x-2">
             <p className="my-2 font-medium">Connected Address:</p>
@@ -28,9 +31,9 @@ const Home: NextPage = () => {
             </code>
           </p>
           <p className="text-center text-lg">
-            Edit your smart contract{" "}
+            Edit your secp256r1 based ERC1056 smart contract{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
+              EthereumDIDRegistry.sol
             </code>{" "}
             in{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
@@ -42,27 +45,34 @@ const Home: NextPage = () => {
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
+              <SiEthereum size={50} />
               <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
+                <Link href="/issuer" passHref className="link">
+                  DID Issuer App
                 </Link>{" "}
-                tab.
               </p>
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
+              <FaUsb size={50} />
               <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
+                <Link href="/signer" passHref className="link">
+                  DID Delegate Signer App
                 </Link>{" "}
-                tab.
+              </p>
+            </div>
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <SiEthereum size={50} />
+              <p>
+                <Link href="/issuer" passHref className="link">
+                  DID Verifier App
+                </Link>{" "}
               </p>
             </div>
           </div>
         </div>
+      </div>
+      <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
+        <Image src="/puf-did-modular.png" alt="Example Image" width={1000} height={600} />
       </div>
     </>
   );
