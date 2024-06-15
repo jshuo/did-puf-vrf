2cf6e7d7d938058ea82a1d64de3fe0effe71a954
Jeff Shuo <jeffshuo@secuxtech.com>
2024-06-15 10:05:57 +0800

    wip from converting Vanilla JS to nextjs

--- packages/hardhat/contracts/EthereumDIDRegistry.sol
+++ packages/hardhat/contracts/EthereumDIDRegistry.sol
@@ -3,67 +3,57 @@
 pragma solidity ^0.8.6;
 
 contract EthereumDIDRegistry {
-	mapping(address => address) public owners;
-	mapping(address => mapping(bytes32 => mapping(bytes32 => uint)))
-		public delegates;
-	mapping(address => uint) public changed;
-	mapping(address => uint) public nonce;
 
-  address constant P256VERIFY_PRECOMPILE = 0x0000000000000000000000000000000000000100;
+  mapping(address => address) public owners;
+  mapping(address => mapping(bytes32 => mapping(address => uint))) public delegates;
+  mapping(address => uint) public changed;
+  mapping(address => uint) public nonce;
+ address constant P256VERIFY_PRECOMPILE = 0x0000000000000000000000000000000000000100;
 
+  modifier onlyOwner(address identity, address actor) {
+    require (actor == identityOwner(identity), "bad_actor");
+    _;
+  }
+event P256VerifyResult(uint256 returnValue);
+  event DIDOwnerChanged(
+    address indexed identity,
+    address owner,
+    uint previousChange
+  );
 
-	modifier onlyOwner(address identity, address actor) {
-		require(actor == identityOwner(identity), "bad_actor");
-		_;
-	}
- event P256VerifyResult(uint256 returnValue);
+  event DIDDelegateChanged(
+    address indexed identity,
+    bytes32 delegateType,
+    address delegate,
+    uint validTo,
+	bytes32 delegatePublicKey,
+    uint previousChange
+  );
 
-	event DIDOwnerChanged(
-		address indexed identity,
-		address owner,
-		uint previousChange
-	);
+  event DIDAttributeChanged(
+    address indexed identity,
+    bytes32 name,
+    bytes value,
+    uint validTo,
+    uint previousChange
+  );
 
-	event DIDDelegateChanged(
-		address indexed identity,
-		bytes32 delegateType,
-		bytes32 delegate,
-		uint validTo,
-		uint previousChange
-	);
+  function identityOwner(address identity) public view returns(address) {
+     address owner = owners[identity];
+     if (owner != address(0x00)) {
+       return owner;
+     }
+     return identity;
+  }
 
-	event DIDAttributeChanged(
-		address indexed identity,
-		bytes32 name,
-		bytes value,
-		uint validTo,
-		uint previousChange
-	);
+  function checkSignature(address identity, uint8 sigV, bytes32 sigR, bytes32 sigS, bytes32 hash) internal returns(address) {
+    address signer = ecrecover(hash, sigV, sigR, sigS);
+    require(signer == identityOwner(identity), "bad_signature");
+    nonce[signer]++;
+    return signer;
+  }
 
-	function identityOwner(address identity) public view returns (address) {
-		address owner = owners[identity];
-		if (owner != address(0x00)) {
-			return owner;
-		}
-		return identity;
-	}
-
-	function checkSignature(
-		address identity,
-		uint8 sigV,
-		bytes32 sigR,
-		bytes32 sigS,
-		bytes32 hash
-	) internal returns (address) {
-		address signer = ecrecover(hash, sigV, sigR, sigS);
-		require(signer == identityOwner(identity), "bad_signature");
-		nonce[signer]++;
-		return signer;
-	}
-
-
-
-	function callP256Verify() public returns (uint256 returnValue) {
+function callP256Verify() public returns (uint256 returnValue) {
       bool success; 
     	bytes memory CALLDATA =
 		  hex"4cee90eb86eaa050036147a12d49004b6b9c72bd725d39d4785011fe190f0b4da73bd4903f0ce3b639bbbf6e8e80d16931ff4bcf5993d58468e8fb19086e8cac36dbcd03009df8c59286b162af3bd7fcc0450c9aa81be5d10d312af6c66b1d604aebd3099c618202fcfe16ae7770b0c49ab5eadf74b754204a3bb6060e44eff37618b065f9832de4ca6ca971a7a1adc826d0f7c00181a5fb2ddf79ae00b4e10e";
@@ -77,230 +67,83 @@ contract EthereumDIDRegistry {
 
 		    emit P256VerifyResult(returnValue);
 	}
-  
-	function validDelegate(
-		address identity,
-		bytes32 delegateType,
-		bytes memory compressedPublicKey
-	) public view returns (bool) {
-		bytes32 publicKey;
-		for (uint i = 1; i <= 32; i++) {
-			publicKey |= bytes32(compressedPublicKey[i] & 0xFF) >> (i * 8);
-		}
-		uint validity = delegates[identity][
-			keccak256(abi.encode(delegateType))
-		][publicKey];
-		return (validity > block.timestamp);
-	}
+	  function validDelegate(address identity, bytes32 delegateType, address delegate ) public view returns(bool) {
+    uint validity = delegates[identity][keccak256(abi.encode(delegateType))][delegate];
+    return (validity > block.timestamp);
+  }
 
-	function changeOwner(
-		address identity,
-		address actor,
-		address newOwner
-	) internal onlyOwner(identity, actor) {
-		owners[identity] = newOwner;
-		emit DIDOwnerChanged(identity, newOwner, changed[identity]);
-		changed[identity] = block.number;
-	}
+  function changeOwner(address identity, address actor, address newOwner) internal onlyOwner(identity, actor) {
+    owners[identity] = newOwner;
+    emit DIDOwnerChanged(identity, newOwner, changed[identity]);
+    changed[identity] = block.number;
+  }
 
-	function changeOwner(address identity, address newOwner) public {
-		changeOwner(identity, msg.sender, newOwner);
-	}
+  function changeOwner(address identity, address newOwner) public {
+    changeOwner(identity, msg.sender, newOwner);
+  }
 
-	function changeOwnerSigned(
-		address identity,
-		uint8 sigV,
-		bytes32 sigR,
-		bytes32 sigS,
-		address newOwner
-	) public {
-		bytes32 hash = keccak256(
-			abi.encodePacked(
-				bytes1(0x19),
-				bytes1(0),
-				this,
-				nonce[identityOwner(identity)],
-				identity,
-				"changeOwner",
-				newOwner
-			)
-		);
-		changeOwner(
-			identity,
-			checkSignature(identity, sigV, sigR, sigS, hash),
-			newOwner
-		);
-	}
+  function changeOwnerSigned(address identity, uint8 sigV, bytes32 sigR, bytes32 sigS, address newOwner) public {
+    bytes32 hash = keccak256(abi.encodePacked(bytes1(0x19), bytes1(0), this, nonce[identityOwner(identity)], identity, "changeOwner", newOwner));
+    changeOwner(identity, checkSignature(identity, sigV, sigR, sigS, hash), newOwner);
+  }
 
-	function addDelegate(
-		address identity,
-		address actor,
-		bytes32 delegateType,
-		bytes memory compressedPublicKey,
-		uint validity
-	) internal onlyOwner(identity, actor) {
-		bytes32 publicKey;
-		for (uint i = 1; i <= 32; i++) {
-			publicKey |= bytes32(compressedPublicKey[i] & 0xFF) >> (i * 8);
-		}
-		delegates[identity][keccak256(abi.encode(delegateType))][publicKey] =
-			block.timestamp +
-			validity;
-		emit DIDDelegateChanged(
-			identity,
-			delegateType,
-			publicKey,
-			block.timestamp + validity,
-			changed[identity]
-		);
-		changed[identity] = block.number;
-	}
+  function addDelegate(address identity, address actor, bytes32 delegateType, address delegate, uint validity, bytes32 delegatePublicKey) internal onlyOwner(identity, actor) {		
 
-	function addDelegate(
-		address DIDIssuerAddress,
-		bytes32 delegateType,
-		bytes memory SignerCompressedPublicKey,
-		uint validity
-	) public {
-		addDelegate(DIDIssuerAddress, msg.sender, delegateType, SignerCompressedPublicKey, validity);
-	}
+	delegates[identity][keccak256(abi.encode(delegateType))][delegate] = block.timestamp + validity;
+    emit DIDDelegateChanged(identity, delegateType, delegate, block.timestamp + validity, delegatePublicKey, changed[identity]);
+    changed[identity] = block.number;
+  }
 
-	function revokeDelegate(
-		address identity,
-		address actor,
-		bytes32 delegateType,
-		bytes memory compressedPublicKey
-	) internal onlyOwner(identity, actor) {
-		bytes32 publicKey;
-		for (uint i = 1; i <= 32; i++) {
-			publicKey |= bytes32(compressedPublicKey[i] & 0xFF) >> (i * 8);
-		}
-		delegates[identity][keccak256(abi.encode(delegateType))][
-			publicKey
-		] = block.timestamp;
-		emit DIDDelegateChanged(
-			identity,
-			delegateType,
-			publicKey,
-			block.timestamp,
-			changed[identity]
-		);
-		changed[identity] = block.number;
-	}
+  function addDelegate(address identity, bytes32 delegateType, address delegate, uint validity, bytes32 delegatePublicKey) public {
+    addDelegate(identity, msg.sender, delegateType, delegate, validity, delegatePublicKey) ;
+  }
 
-	function revokeDelegate(
-		address identity,
-		bytes32 delegateType,
-		bytes memory compressedPublicKey
-	) public {
-		revokeDelegate(identity, msg.sender, delegateType, compressedPublicKey);
-	}
+  function addDelegateSigned(address identity, uint8 sigV, bytes32 sigR, bytes32 sigS, bytes32 delegateType, address delegate, uint validity, bytes32 delegatePublicKey) public {
+    bytes32 hash = keccak256(abi.encodePacked(bytes1(0x19), bytes1(0), this, nonce[identityOwner(identity)], identity, "addDelegate", delegateType, delegate, validity));
+    addDelegate(identity, checkSignature(identity, sigV, sigR, sigS, hash), delegateType, delegate,  validity, delegatePublicKey);
+  }
 
-	// function revokeDelegateSigned(address identity, uint8 sigV, bytes32 sigR, bytes32 sigS, bytes32 delegateType, address delegate) public {
-	//   bytes32 hash = keccak256(abi.encodePacked(bytes1(0x19), bytes1(0), this, nonce[identityOwner(identity)], identity, "revokeDelegate", delegateType, delegate));
-	//   revokeDelegate(identity, checkSignature(identity, sigV, sigR, sigS, hash), delegateType, delegate);
-	// }
+  function revokeDelegate(address identity, address actor, bytes32 delegateType, address delegate, bytes32 delegatePublicKey) internal onlyOwner(identity, actor) {
+    delegates[identity][keccak256(abi.encode(delegateType))][delegate] = block.timestamp;
+    emit DIDDelegateChanged(identity, delegateType, delegate, block.timestamp, delegatePublicKey, changed[identity]);
+    changed[identity] = block.number;
+  }
 
-	function setAttribute(
-		address identity,
-		address actor,
-		bytes32 name,
-		bytes memory value,
-		uint validity
-	) internal onlyOwner(identity, actor) {
-		emit DIDAttributeChanged(
-			identity,
-			name,
-			value,
-			block.timestamp + validity,
-			changed[identity]
-		);
-		changed[identity] = block.number;
-	}
+  function revokeDelegate(address identity, bytes32 delegateType, address delegate, bytes32 delegatePublicKey) public {
+    revokeDelegate(identity, msg.sender, delegateType, delegate, delegatePublicKey);
+  }
 
-	function setAttribute(
-		address identity,
-		bytes32 name,
-		bytes memory value,
-		uint validity
-	) public {
-		setAttribute(identity, msg.sender, name, value, validity);
-	}
+  function revokeDelegateSigned(address identity, uint8 sigV, bytes32 sigR, bytes32 sigS, bytes32 delegateType, address delegate, bytes32 delegatePublicKey) public {
+    bytes32 hash = keccak256(abi.encodePacked(bytes1(0x19), bytes1(0), this, nonce[identityOwner(identity)], identity, "revokeDelegate", delegateType, delegate));
+    revokeDelegate(identity, checkSignature(identity, sigV, sigR, sigS, hash), delegateType, delegate, delegatePublicKey);
+  }
 
-	function setAttributeSigned(
-		address identity,
-		uint8 sigV,
-		bytes32 sigR,
-		bytes32 sigS,
-		bytes32 name,
-		bytes memory value,
-		uint validity
-	) public {
-		bytes32 hash = keccak256(
-			abi.encodePacked(
-				bytes1(0x19),
-				bytes1(0),
-				this,
-				nonce[identityOwner(identity)],
-				identity,
-				"setAttribute",
-				name,
-				value,
-				validity
-			)
-		);
-		setAttribute(
-			identity,
-			checkSignature(identity, sigV, sigR, sigS, hash),
-			name,
-			value,
-			validity
-		);
-	}
+  function setAttribute(address identity, address actor, bytes32 name, bytes memory value, uint validity ) internal onlyOwner(identity, actor) {
+    emit DIDAttributeChanged(identity, name, value, block.timestamp + validity, changed[identity]);
+    changed[identity] = block.number;
+  }
 
-	function revokeAttribute(
-		address identity,
-		address actor,
-		bytes32 name,
-		bytes memory value
-	) internal onlyOwner(identity, actor) {
-		emit DIDAttributeChanged(identity, name, value, 0, changed[identity]);
-		changed[identity] = block.number;
-	}
+  function setAttribute(address identity, bytes32 name, bytes memory value, uint validity) public {
+    setAttribute(identity, msg.sender, name, value, validity);
+  }
 
-	function revokeAttribute(
-		address identity,
-		bytes32 name,
-		bytes memory value
-	) public {
-		revokeAttribute(identity, msg.sender, name, value);
-	}
+  function setAttributeSigned(address identity, uint8 sigV, bytes32 sigR, bytes32 sigS, bytes32 name, bytes memory value, uint validity) public {
+    bytes32 hash = keccak256(abi.encodePacked(bytes1(0x19), bytes1(0), this, nonce[identityOwner(identity)], identity, "setAttribute", name, value, validity));
+    setAttribute(identity, checkSignature(identity, sigV, sigR, sigS, hash), name, value, validity);
+  }
 
-	function revokeAttributeSigned(
-		address identity,
-		uint8 sigV,
-		bytes32 sigR,
-		bytes32 sigS,
-		bytes32 name,
-		bytes memory value
-	) public {
-		bytes32 hash = keccak256(
-			abi.encodePacked(
-				bytes1(0x19),
-				bytes1(0),
-				this,
-				nonce[identityOwner(identity)],
-				identity,
-				"revokeAttribute",
-				name,
-				value
-			)
-		);
-		revokeAttribute(
-			identity,
-			checkSignature(identity, sigV, sigR, sigS, hash),
-			name,
-			value
-		);
-	}
+  function revokeAttribute(address identity, address actor, bytes32 name, bytes memory value ) internal onlyOwner(identity, actor) {
+    emit DIDAttributeChanged(identity, name, value, 0, changed[identity]);
+    changed[identity] = block.number;
+  }
+
+  function revokeAttribute(address identity, bytes32 name, bytes memory value) public {
+    revokeAttribute(identity, msg.sender, name, value);
+  }
+
+  function revokeAttributeSigned(address identity, uint8 sigV, bytes32 sigR, bytes32 sigS, bytes32 name, bytes memory value) public {
+    bytes32 hash = keccak256(abi.encodePacked(bytes1(0x19), bytes1(0), this, nonce[identityOwner(identity)], identity, "revokeAttribute", name, value));
+    revokeAttribute(identity, checkSignature(identity, sigV, sigR, sigS, hash), name, value);
+  }
+
 }
--- packages/nextjs/app/issuer/page.tsx
+++ packages/nextjs/app/issuer/page.tsx
@@ -1,10 +1,136 @@
 "use client";
 
+import { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import { Account } from "../../components/Account";
+import { useEthersProvider } from "../ethers/useEthersProvider";
+import { useEthersSigner } from "../ethers/useEthersSigner";
+import * as didJWT from "did-jwt";
+import { Resolver } from "did-resolver";
+import { EthrDID } from "ethr-did";
+import { getResolver } from "ethr-did-resolver";
+
+type unsignedJWT = {
+  payload: didJWT.JWTPayload;
+  options?: {
+    issuer: string; // Removed signer in order to be able to prepare the message
+  };
+  header?: {
+    alg: string;
+  };
+};
 
 export default function IssuerPage() {
+  const [subjectAddress, setSubjectAddress] = useState("");
+  const [audienceAddress, setAudienceAddress] = useState("");
+  const [subjectDID, setSubjectDID] = useState("");
+  const [audienceDID, setAudienceDID] = useState("");
+  const [privateClaim, setPrivateClaim] = useState("");
+  const [issuerDID, setIssuerDID] = useState("");
+  const [delegateSigner, setDelegateSigner] = useState("");
+  const [delegateSignerIdentifier, setDelegateSignerIdentifier] = useState("");
+  const [connectedMetamaskAccount, setConnectedMetamaskAccount] = useState("");
+  const [signedJWT, setSignedJWT] = useState("");
+  let JWTMessage: unsignedJWT; // Unsigned JWT message
+  const [issuerDid, setIssuerDid] = useState(null);
+  const chainId = 80002;
+  const provider = useEthersProvider({ chainId });
+  const signer = useEthersSigner({ chainId });
+  const registryAddress = "0x0B306BF915C4d645ff596e518fAf3F9669b97016";
+  const providerConfig = {
+    networks: [{ name: "0x13882", provider }],
+    registry: registryAddress,
+  };
+  const ethrDidResolver = getResolver(providerConfig);
+  const didResolver = new Resolver(ethrDidResolver);
+  let issuerAddress: string, chainNameOrId: number;
+
+  const processDid = async () => {
+    const chainNameOrId = chainId;
+    const subjectAddr = subjectAddress || "0xDBB3d90156fC23c28C709eB68af8403836951AF8";
+    const audienceAddr = audienceAddress || "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
+
+    const subjectDid = new EthrDID({ identifier: subjectAddr, provider, chainNameOrId });
+    const audienceDid = new EthrDID({ identifier: audienceAddr, provider, chainNameOrId });
+
+    setSubjectDID(subjectDid.did);
+    setAudienceDID(audienceDid.did);
+  };
+
+  const prepareJWT = async () => {
+    if (signer) {
+      issuerAddress = await signer.getAddress();
+      chainNameOrId = await signer.getChainId();
+    }
+
+    const issuerDid = new EthrDID({ identifier: issuerAddress, provider, chainNameOrId });
+
+    const buildJWT = {
+      payload: {
+        iss: issuerDid.did,
+        sub: subjectDID,
+        aud: audienceDID,
+        privateClaim: privateClaim || "DEFAULT_PRIVATE_CLAIM",
+      },
+    };
+
+    setIssuerDID(issuerDid.did);
+    JWTMessage = buildJWT;
+  };
+
+  const createDelegate = async () => {
+    if (signer) {
+      issuerAddress = await signer.getAddress();
+      chainNameOrId = await signer.getChainId();
+    }
+
+    const issuerDid = new EthrDID({
+      identifier: issuerAddress,
+      provider,
+      chainNameOrId,
+      txSigner: signer,
+      alg: "ES256K",
+    });
+
+    const { kp } = await issuerDid.createSigningDelegate();
+    setDelegateSigner(kp.address);
+    setDelegateSignerIdentifier(kp.identifier);
+  };
+
+  const signJWT = async () => {
+    if (!JWTMessage) return;
+
+    if (signer) {
+      issuerAddress = await signer.getAddress();
+      chainNameOrId = await signer.getChainId();
+
+      const issuerDid = new EthrDID({
+        identifier: issuerAddress,
+        provider,
+        chainNameOrId,
+        txSigner: signer,
+        alg: "ES256K",
+      });
+      const signedJWT = await issuerDid.signJWT(JWTMessage.payload);
+
+      setSignedJWT(signedJWT);
+
+      setConnectedMetamaskAccount(await signer.getAddress());
+
+      const issuerDoc = await didResolver.resolve(issuerDid.did);
+      console.debug(issuerDoc);
+    }
+    await fetch("/api/saveJWT", {
+      method: "POST",
+      body: JSON.stringify({ signedJWT }),
+      headers: { "Content-type": "application/json" },
+    }).then(async res => {
+      const message = await res.text();
+      console.log(JSON.parse(message).message);
+    });
+  };
+
   return (
     <div>
       <Head>
@@ -31,16 +157,56 @@ export default function IssuerPage() {
                 </li>
                 <li>3. Register the binding to ERC1056 smart contract</li>
               </ol>
-              <button className="btn btn-blue" id="createDelegate">
-                Create Delegate
-              </button>
-
-              <h3>
-                Delegate Signer SECP256R1 (or P-256) Public key: <span id="delegateSigner"></span>
-              </h3>
-              <h3>
-                Delegate Signer Identifier: <span id="delegateSignerIdentifier"></span>
-              </h3>
+              <h1>JWT Handling with DID</h1>
+              <section>
+                <h2>Configure subject and audience DIDs</h2>
+                <form
+                  onSubmit={e => {
+                    e.preventDefault();
+                    processDid();
+                  }}
+                >
+                  <label>
+                    Subject Address:
+                    <input type="text" value={subjectAddress} onChange={e => setSubjectAddress(e.target.value)} />
+                  </label>
+                  <label>
+                    Audience Address:
+                    <input type="text" value={audienceAddress} onChange={e => setAudienceAddress(e.target.value)} />
+                  </label>
+                  <button type="submit">Configure Addresses</button>
+                </form>
+                <span id="subjectDID">{subjectDID}</span>
+                <span id="audienceDID">{audienceDID}</span>
+              </section>
+              <section>
+                <h2>Prepare JWT Token for Signing</h2>
+                <form
+                  onSubmit={e => {
+                    e.preventDefault();
+                    prepareJWT();
+                  }}
+                >
+                  <label>
+                    Private Claim:
+                    <input type="text" value={privateClaim} onChange={e => setPrivateClaim(e.target.value)} />
+                  </label>
+                  <button type="submit">Prepare JWT</button>
+                </form>
+                <span id="issuerDID">{issuerDID}</span>
+              </section>
+              <section>
+                <h2>Create Signing Delegate</h2>
+                <button onClick={createDelegate}>Create Delegate</button>
+                <span id="delegateSigner">{delegateSigner}</span>
+                <span id="delegateSignerIdentifier">{delegateSignerIdentifier}</span>
+              </section>
+              <section>
+                <h2>Sign JWT Token</h2>
+                <button onClick={signJWT}>Sign JWT</button>
+                <span id="connectedMetamaskAccount">{connectedMetamaskAccount}</span>
+                <span id="signedJWT">{signedJWT}</span>
+              </section>
             </span>
           </h2>
         </div>
