e6eebe7696528d017a0ef2b5f759ddd1284d7ac6
Jeff Shuo <jeffshuo@secuxtech.com>
2024-07-06 11:26:33 +0800

    testing

--- packages/nextjs/app/api/services/route.js
+++ packages/nextjs/app/api/services/route.js
@@ -1,35 +1,35 @@
-import OpenAI from "openai";
+// import OpenAI from "openai";
 
-const openai = new OpenAI({
-  apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
-});
+// const openai = new OpenAI({
+//   apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
+// });
 
 
 export async function POST(request) {
   const requestData = await request.json();
   const requestContent = requestData.content; // Assuming the request has a 'content' field
 
-  console.log(JSON.stringify(requestContent));
-  // 2. Formulate Summarization Prompt
-  const prompt = `
-Please review the details to determine if there is any indication that the item is manufactured in Taiwan. If it is, please respond with "Originating from Taiwan"; otherwise, provide an alternative response. :\n\n${JSON.stringify(
-    requestContent,
-  )}`;
+//   console.log(JSON.stringify(requestContent));
+//   // 2. Formulate Summarization Prompt
+//   const prompt = `
+// Please review the details to determine if there is any indication that the item is manufactured in Taiwan. If it is, please respond with "Originating from Taiwan"; otherwise, provide an alternative response. :\n\n${JSON.stringify(
+//     requestContent,
+//   )}`;
 
-  // 3. Send Request to OpenAI
-  // model: "gpt-4",
-  const response = await openai.chat.completions.create({
-    model: "gpt-4",
-    messages: [{ role: "user", content: prompt }],
-  });
+//   // 3. Send Request to OpenAI
+//   // model: "gpt-4",
+//   const response = await openai.chat.completions.create({
+//     model: "gpt-4",
+//     messages: [{ role: "user", content: prompt }],
+//   });
 
-  // 4. Return Summary
-  const summary = response.choices[0].message.content;
-  // console.log(summary);
-  // Perform your desired server-side logic with the data
-  // For example, save it to a database or send it to another API
+//   // 4. Return Summary
+//   const summary = response.choices[0].message.content;
+//   // console.log(summary);
+//   // Perform your desired server-side logic with the data
+//   // For example, save it to a database or send it to another API
 
-  return new Response(JSON.stringify({ message: summary }), {
+  return new Response(JSON.stringify({ message: "test is a testing" }), {
     status: 200, // Or an appropriate status code
     headers: { "Content-Type": "application/json" },
   });
