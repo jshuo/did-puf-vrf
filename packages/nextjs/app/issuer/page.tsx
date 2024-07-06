b44bc1dd34e3a120e0d6e57ce2de446f633bf6c1
Jeff Shuo <jeffshuo@secuxtech.com>
2024-07-06 15:23:24 +0800

    added  expirein and loading animation to issuer page

--- packages/nextjs/app/issuer/page.tsx
+++ packages/nextjs/app/issuer/page.tsx
@@ -12,6 +12,14 @@ import { useTargetNetwork } from "~~/hooks/scaffold-eth/useTargetNetwork";
 export default function IssuerPage() {
   const [delegateSigner, setDelegateSigner] = useState("");
   const [delegateSignerIdentifier, setDelegateSignerIdentifier] = useState("");
+  const [expireIn, setExpireIn] = useState('');
+
+  const [loading, setLoading] = useState(false);
+
+
+  const handleInputChange = (e) => {
+    setExpireIn(e.target.value);
+  };
   const provider = useEthersProvider();
   const signer = useEthersSigner();
   const hardHatRegistryAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // hardhat localhost
@@ -21,7 +29,14 @@ export default function IssuerPage() {
 
   let issuerAddress: string, chainNameOrId: number;
 
-  const createDelegate = async () => {
+  const createDelegate = async (expiresIn: string) => {
+    enum DelegateTypes {
+      veriKey = 'veriKey',
+      sigAuth = 'sigAuth',
+      enc = 'enc',
+    }
+
+    setLoading(true); // Set loading state to true
     if (signer) {
       let registryAddress;
       if (targetNetwork.id == 80002) {
@@ -41,11 +56,12 @@ export default function IssuerPage() {
         txSigner: signer,
         alg: "ES256K",
       });
-      const { kp, txHash } = (await issuerDid?.createSigningDelegate()) || { kp: undefined };
+      const { kp, txHash } = (await issuerDid?.createSigningDelegate(DelegateTypes.veriKey, parseInt(expiresIn))) || { kp: undefined };
       if (kp !== undefined) {
         console.log(txHash);
         setDelegateSigner(kp.address);
         setDelegateSignerIdentifier(kp.identifier);
+        setLoading(false); // Set loading state to false after processing
       }
     }
   };
@@ -69,6 +85,7 @@ export default function IssuerPage() {
               </h2>
               <p></p>
               <hr />
+
               <ol>
                 <li>1. Create a NeoPUF USB Dongle signing delegate for the connected DID Issuer Account</li>
                 <li>
@@ -76,42 +93,76 @@ export default function IssuerPage() {
                 </li>
                 <li>3. Register the binding to ERC1056 smart contract</li>
               </ol>
+              <hr />
 
+              <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
+                <Image src="/puf-did-ai-board.png" alt="Example Image" width={550} height={450} />
+                <Image src="/puf-did-diagram.png" alt="Example Image" width={600} height={450} />
+
+              </div>
               <section>
-                <button
-                  onClick={createDelegate}
-                  style={{
-                    backgroundColor: "#007BFF",
-                    color: "white",
-                    border: "none",
-                    borderRadius: "4px",
-                    padding: "10px 20px",
-                    cursor: "pointer",
-                    fontSize: "16px",
-                  }}
-                  onMouseOver={e => {
-                    e.target.style.backgroundColor = "#0056b3";
-                  }}
-                  onMouseOut={e => {
-                    e.target.style.backgroundColor = "#007BFF";
-                  }}
-                >
-                  Create Delegate
-                </button>
+                <label className="block text-2xl font-bold"> Create Delegate Signer w/Expiration (seconds)
+                  <br />
+                  <button
+                    onClick={() => createDelegate(expireIn)}
+                    style={{
+                      backgroundColor: "#007BFF",
+                      color: "white",
+                      border: "none",
+                      borderRadius: "4px",
+                      padding: "10px 20px",
+                      cursor: "pointer",
+                      fontSize: "16px",
+                    }}
+                    onMouseOver={e => {
+                      e.target.style.backgroundColor = "#0056b3";
+                    }}
+                    onMouseOut={e => {
+                      e.target.style.backgroundColor = "#007BFF";
+                    }}
+                  >
+                    Create Delegate
+                  </button>
+
+                  <input
+                    type="text"
+                    value={expireIn}
+                    onChange={handleInputChange}
+                    placeholder=""
+                    style={{
+                      width: "80px", // Adjust width as needed
+                      marginBottom: "10px",
+                      padding: "10px",
+                      fontSize: "16px",
+                      borderRadius: "4px",
+                      border: "1px solid #ccc",
+                    }}
+                  />
+                </label>
                 <section>
                 </section>
                 <br />
                 <span
                   id="delegateSigner" className="block text-2xl font-bold"            >
-                  delegate signer secp256 r1 address: 
+                  delegate signer secp256 r1 address:
                 </span>
-                {delegateSigner}
+                {loading ? (
+                  <div className="loading-spinner">Loading...</div> // Replace with your loading spinner or animation component
+                ) : (
+                  <div className="block text-2xl" > {delegateSigner}</div>
+                )}
+
                 <br />
                 <span
-                 id="delegateSigner" className="block text-2xl font-bold"            >
-                  delegate signer secp256 r1 public key: 
+                  id="delegateSigner" className="block text-2xl font-bold"            >
+                  delegate signer secp256 r1 public key:
                 </span>
-                {delegateSignerIdentifier}
+                {loading ? (
+                  <div className="loading-spinner">Loading...</div> // Replace with your loading spinner or animation component
+                ) : (
+                  <div className="block text-2xl" >  {delegateSignerIdentifier}</div>
+                )}
+
                 <br />
               </section>
             </span>
@@ -122,10 +173,7 @@ export default function IssuerPage() {
         <br />
       </main>
 
-      <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-        <Image src="/puf-did-diagram.png" alt="Example Image" width={800} height={600} />
-        <Image src="/puf-did-ai-board.png" alt="Example Image" width={800} height={600} />
-      </div>
+
     </div>
   );
 }
